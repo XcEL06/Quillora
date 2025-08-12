1aa5eb1e3f974da07e5c6ede8ea4c2efcbab3816
XcEL06 <careerevive@gmail.com>
2025-08-12 13:21:08 +0100

    Create messageController.js

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,15 @@
+=== FILE: backend/controllers/messageController.js ===
+const Message = require('../models/Message');
+exports.sendMessage = async (req,res)=>{
+  const {to, text, conversationId} = req.body;
+  const msg = await Message.create({from: req.user._id, to, text, conversationId: conversationId || null});
+  // optionally notify via socket
+  const io = req.app.get('io');
+  io && io.to('user_' + String(to)).emit('receive_message', {from: req.user._id, to, text, _id: msg._id});
+  res.json(msg);
+};
+exports.getConversation = async (req,res)=>{
+  const {userId} = req.params;
+  const messages = await Message.find({$or:[{from:req.user._id,to:userId},{from:userId,to:req.user._id}]}).sort({createdAt:1});
+  res.json(messages);
+};
