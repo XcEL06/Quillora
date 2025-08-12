91fc4329d80f6a3596969ce594022680b715ddcb
XcEL06 <careerevive@gmail.com>
2025-08-12 13:38:05 +0100

    Create groupRoutes.js

---
+++ backend/routes/groupRoutes.js
@@ -0,0 +1,9 @@
+=== FILE: backend/routes/groupRoutes.js ===
+const express = require('express');
+const router = express.Router();
+const {protect} = require('../middleware/auth');
+const { createGroup, joinGroup, getGroups } = require('../controllers/groupController');
+router.post('/', protect, createGroup);
+router.post('/:id/join', protect, joinGroup);
+router.get('/', getGroups);
+module.exports = router;
