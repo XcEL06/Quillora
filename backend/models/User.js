4fa72825c62f65275cbab0acbefe2a99810562a8
XcEL06 <careerevive@gmail.com>
2025-08-12 01:44:00 +0100

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+=== FILE: backend/models/User.js ===
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  username: {type: String, required:true, unique:true},
+  email: {type:String, required:true, unique:true},
+  password: {type:String, required:true},
+  bio: {type:String, default:''},
+  avatar: {type:String, default:''},
+  followers: [{type: mongoose.Schema.Types.ObjectId, ref:'User'}],
+  following: [{type: mongoose.Schema.Types.ObjectId, ref:'User'}],
+  blocked: [{type: mongoose.Schema.Types.ObjectId, ref:'User'}],
+  muted: [{type: mongoose.Schema.Types.ObjectId, ref:'User'}],
+  role: {type:String, enum:['user','admin'], default:'user'},
+  isBanned: {type:Boolean, default:false}
+}, {timestamps:true});
+
+UserSchema.pre('save', async function(next){
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.matchPassword = function(password) {
+  return bcrypt.compare(password, this.password);
+}
+
+module.exports = mongoose.model('User', UserSchema);
