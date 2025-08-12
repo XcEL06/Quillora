dfac5ebb96fb72bc268f43079482112df2cbe966
XcEL06 <careerevive@gmail.com>
2025-08-12 14:37:45 +0100

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,3 @@
+=== FILE: frontend/src/pages/Home.js ===
+import React from 'react';
+export default function Home(){ return (<div className='container'><h1>Welcome to Quillora</h1><p>Connect with writers, poets and musicians.</p></div>);}
