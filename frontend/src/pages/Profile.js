7325f58e57c3a20d5201d23a8d1d9d8e0f025e16
XcEL06 <careerevive@gmail.com>
2025-08-12 14:41:17 +0100

    Create Profile.js

---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,11 @@
+=== FILE: frontend/src/pages/Profile.js ===
+import React, {useEffect,useState} from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+export default function Profile(){
+  const {id} = useParams();
+  const [user,setUser]=useState(null);
+  useEffect(()=>{ axios.get('http://localhost:5000/api/users/profile/'+id).then(r=>setUser(r.data)); },[id]);
+  if(!user) return <div className='container'>Loading...</div>;
+  return (<div className='container'><h2>{user.username}</h2><p>{user.bio}</p></div>);
+}
