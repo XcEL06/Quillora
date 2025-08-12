c73beeab176494fa27625c936188fdaff110baaf
XcEL06 <careerevive@gmail.com>
2025-08-12 13:23:57 +0100

    Create reportController.js

---
+++ backend/controllers/reportController.js
@@ -0,0 +1,7 @@
+=== FILE: backend/controllers/reportController.js ===
+const Report = require('../models/Report');
+exports.createReport = async (req,res)=>{
+  const {targetType,targetId,reason} = req.body;
+  const r = await Report.create({reporter:req.user._id,targetType,targetId,reason});
+  res.json(r);
+};
