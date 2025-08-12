cbf08e0fa11ab81137aaa11a7f08c38b408e284e
XcEL06 <careerevive@gmail.com>
2025-08-12 14:40:20 +0100

    Create Feed.js

---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,8 @@
+=== FILE: frontend/src/pages/Feed.js ===
+import React, {useEffect,useState} from 'react';
+import axios from 'axios';
+export default function Feed(){
+  const [posts,setPosts]=useState([]);
+  useEffect(()=>{ axios.get('http://localhost:5000/api/posts/feed').then(r=>setPosts(r.data)); },[]);
+  return (<div className='container'>{posts.map(p=> (<div key={p._id} className='card'><h4>{p.author.username}</h4><p>{p.content}</p>{p.mediaUrl && <audio controls src={'http://localhost:5000'+p.mediaUrl}></audio>}</div>))}</div>);
+}
