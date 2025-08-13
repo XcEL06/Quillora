d8f9184e61412969d06d22349fe35ee06557a8d4
XcEL06 <careerevive@gmail.com>
2025-08-13 15:17:41 +0100

    Update Admin.js

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,2 +1,2 @@
 import React,{useEffect,useState} from 'react'; import axios from 'axios';
-export default function Admin(){ const [reports,setReports]=useState([]); useEffect(()=>{ axios.get('http://localhost:5000/api/admin/reports').then(r=>setReports(r.data)).catch(()=>{}); },[]); return (<div className='container'><h2>Admin</h2>{reports.map(r=>(<div key={r._id} className='card'><p>{r.reason}</p></div>))}</div>); }
+export default function Admin(){ const [reports,setReports]=useState([]); useEffect(()=>{ axios.get'https://quillora-5ddm.onrender.com'.then(r=>setReports(r.data)).catch(()=>{}); },[]); return (<div className='container'><h2>Admin</h2>{reports.map(r=>(<div key={r._id} className='card'><p>{r.reason}</p></div>))}</div>); }
