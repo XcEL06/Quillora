7bfc377a78dcc200a4022bbaaa2af49c6b617e27
XcEL06 <careerevive@gmail.com>
2025-08-13 04:39:59 +0100

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,4 +1,3 @@
-=== FILE: backend/utils/generateToken.js ===
 const jwt = require('jsonwebtoken');
 function generateToken(user) {
   return jwt.sign({id: user._id, role: user.role}, process.env.JWT_SECRET, {expiresIn: '30d'});
