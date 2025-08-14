48430488db81ea384691d53e7fa706989853b597
XcEL06 <careerevive@gmail.com>
2025-08-14 01:48:07 +0100

    Update Feed.js

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,7 +1,27 @@
-import React, {useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-export default function Feed(){
-  const [posts,setPosts]=useState([]);
-  useEffect(()=>{ axios.get'https://quillora-5ddm.onrender.com/api/posts/feed'.then(r=>setPosts(r.data)); },[]);
-  return (<div className='container'>{posts.map(p=> (<div key={p._id} className='card'><h4>{p.author.username}</h4><p>{p.content}</p>{p.mediaUrl && <audio controls src={'http://localhost:5000'+p.mediaUrl}></audio>}</div>))}</div>);
-}
+
+export default function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://quillora-5ddm.onrender.com/api/posts/feed')
+      .then(r => setPosts(r.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div className='container'>
+      {posts.map(p => (
+        <div key={p._id} className='card'>
+          <h4>{p.author?.username}</h4>
+          <p>{p.content}</p>
+          {p.mediaUrl && (
+            <audio controls src={`http://localhost:5000${p.mediaUrl}`}></audio>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+  }
